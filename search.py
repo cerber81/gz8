4c5f97bafc7f7f556a100799695d8fc02cf5cd9c
AndrewDar <geologalone@gmail.com>
2022-10-19 16:08:03 +0300

    formatting

--- search.py
+++ search.py
@@ -1,15 +1,12 @@
 def person_search(data_file, name):
-    with open (data_file, 'r', encoding='utf_8') as data:
+    with open(data_file, 'r', encoding='utf_8') as data:
         data_list = data.readlines()
-        length = len(data_list)
-        for i in range(1, length):
+        for i in range(1, len(data_list)):
             if name in data_list[i]:
-                print (data_list[i])
+                print(data_list[i])
                 break
-        else: 
-            print('data not found')    
-            
-        
-        
-person_search('Hogwarts.csv', 'зоолог')        
-        
+        else:
+            print('data not found')
+
+
+#person_search('Hogwarts.csv', 'Квирел')
