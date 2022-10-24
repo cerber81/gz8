21502a6091708b8de15a62e257f00e9483f37315
Tamyrlan <tshurenov@gmail.com>
2022-10-24 15:21:51 +0600

    Работающий апдейт инфы

--- model_update_1.py
+++ model_update_1.py
@@ -9,21 +9,44 @@ def update():
         n_id= input('Enter id of employee that you want to change :')
         print('1. Change Name')
         print('2. Change Position')
-        print('3. Change Owl ')
-        print('4. Change Comments')
+        print('3. Change Subject ')
+        print('4. Change Owl')
+        print('5. Change Comments')
+        n=int(input('Enter the section that you want to change (1,2->5):'))
         found=0
         r=csv.reader(f,delimiter=";")
         nrec =[]
         for rec in r:
             if rec[0] ==n_id:
-                print('Current record:',rec)
-                rec[1] = input("Enter the name:")
-                print('Updated record:',rec)
-                found=1
+                if n==1:
+                    print('Current record:',rec)
+                    rec[1] = input("Enter new name:")
+                    print('Updated record:',rec)
+                    found=1
+                elif n==2:
+                    print('Current record:',rec)
+                    rec[2] = input("Enter new Position:")
+                    print('Updated record:',rec)
+                    found=1
+                elif n==3:
+                    print('Current record:',rec)
+                    rec[3] = input("Enter new subject:")
+                    print('Updated record:',rec)
+                    found=1
+                elif n==4:
+                    print('Current record:',rec)
+                    rec[4] = input("Enter new Owl:")
+                    print('Updated record:',rec)
+                    found=1
+                elif n==5:
+                    print('Current record:',rec)
+                    rec[5] = input("Enter new Comments:")
+                    print('Updated record:',rec)
+                    found=1
             nrec.append(rec)
 
         if found == 0:
-            print('no')
+            print('Error, please try again :(')
             f.close()
         else:
             with open('Hogwarts.csv','w',newline='',encoding='utf-8') as f:
